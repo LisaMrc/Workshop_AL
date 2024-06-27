9e8a19fc648a3bc626a08ce5f24bdd0b84b05673
AnassDoublal <a.doublal02@gmail.com>
2024-06-27 18:04:29 +0200

    [ADD] Fish page V1

--- static/script.js
+++ static/script.js
@@ -3,10 +3,16 @@ fetch('/api/fishes')
 .then(data => {
     const tableBody = document.getElementById('fishes-table');
     data.forEach(fish => {
-        const row = document.createElement('div.oneFish');
+        const row = document.createElement('div');
+        row.className = "oneFish";
         row.innerHTML = `
-            <div>${fish.common_name}</div>
             <div><img src="/static/assets/fish/${fish.img}" alt="${fish.common_name}"></div>
+            <div>${fish.common_name}</div>
+            <div class="card-overlay">
+            <h2>${fish.scientific_name}</h2>
+                <p>${fish.family} family</p>
+                <p>Size: ${fish.average_size} m</p>
+            </div>
         `;
         tableBody.appendChild(row);
     });
